4fc29b5c372247439a7be0db152886f4ccb395b8
Luiz <manuercolle@gmail.com>
2025-04-01 16:24:24 -0300

    compress .csv implement

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,12 @@ package org.example;
 import com.spire.pdf.PdfDocument;
 import com.spire.pdf.utilities.PdfTable;
 import com.spire.pdf.utilities.PdfTableExtractor;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -31,9 +35,28 @@ public class App {
             }
         }
 
-        FileWriter fw = new FileWriter("ExtractTable.csv");
+        String zipFileName = "ExtractTable.csv";
+        FileWriter fw = new FileWriter(zipFileName);
         fw.write(builder.toString());
         fw.flush();
         fw.close();
+
+        FileOutputStream fos = new FileOutputStream("Data.zip");
+        ZipOutputStream zs = new ZipOutputStream(fos);
+
+        FileInputStream fis = new FileInputStream(zipFileName);
+        ZipEntry entry = new ZipEntry(zipFileName);
+        zs.putNextEntry(entry);
+
+        int length;
+        byte[] buffer = new byte[1024];
+
+        while((length = fis.read(buffer)) > 0){
+            zs.write(buffer, 0, length);
+        }
+        fis.close();
+        zs.closeEntry();
+        zs.close();
     }
+
 }
